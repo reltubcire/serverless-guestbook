927573030bc0d775c2cf1a3f0e27d16082d3bc9e
reltubcire <54082833+reltubcire@users.noreply.github.com>
2019-08-16 11:03:41 -0700

    change apiUrl entry to point to API Gateway URL
    Update apiUrl defined at top of file with route URL to API gateway for guestbook cloud function.

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://3e008e74.us-south.apiconnect.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
